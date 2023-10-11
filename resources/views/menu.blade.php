95946cc6e6095a5c618edec444cfd6d716e806ea
ainaplanass <planas.aina@gmail.com>
2023-10-11 10:43:02 +0200

    BootstrapModal

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -73,23 +73,7 @@
                 </div>
             </div>           
         </div>
-    </div>
-    <div class="modal fade" id="noticiaModal1" tabindex="-1" aria-labelledby="noticiaModalLabel1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="noticiaModalLabel1">Título del Modal</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                </div>
-                <div class="modal-body">
-                    Contenido del modal...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
-                </div>
-            </div>
-        </div>
-    </div>    
+    </div>  
 </body>
 </html>
 @endsection
